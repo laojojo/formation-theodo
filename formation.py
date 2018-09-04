9d3cdcc358a0d965d444920f9b43e034d10f18f5
jordanl <jordanl@theodo.fr>
2018-09-04 22:09:36 +0200

    Apply colormap to maskTrace

--- formation.py
+++ formation.py
@@ -9,9 +9,7 @@ if __name__ == "__main__":
     videoName = 'opencv-sample-video.avi'
     video = cv2.VideoCapture(repositoryPath + videoName)
     backgroundSubstractor = cv2.createBackgroundSubtractorMOG2(history=200, varThreshold=128, detectShadows=False)
-    alpha = 0.3
     maskTrace = 0
-    imageCount = 0
 
     while(video.isOpened()):
         isFrame, frame = video.read()
@@ -22,21 +20,13 @@ if __name__ == "__main__":
             maskTrace = maskTrace + mask
 
             heat = applyCustomColorMap(maskTrace, humanFlowColormap)
-            heatmap = frame.copy()
 
-            cv2.addWeighted(heat, alpha, heatmap, 1 - alpha, 0, heatmap)
-
-            cv2.imshow('heatmap', heatmap)
-
-            print('Image number:', imageCount)
+            cv2.imshow('heat', heat)
 
             cv2.imwrite(repositoryPath + 'results/frame.png', frame)
             cv2.imwrite(repositoryPath + 'results/mask.png', mask)
             cv2.imwrite(repositoryPath + 'results/maskTrace.png', maskTrace)
             cv2.imwrite(repositoryPath + 'results/heat.png', heat)
-            cv2.imwrite(repositoryPath + 'results/heatmap.png', heatmap)
-
-            imageCount += 1
 
             if (cv2.waitKey(5) & 0xFF == ord('q')):
                 video.release()
